94626381bb4df61b758b153bdb0f82b8d41f198b
pstnv <pstnv@mail.ru>
2024-04-06 21:17:34 +0500

    first commit

---
+++ models/Job.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const JobSchema = new mongoose.Schema(
+    {
+        company: {
+            type: String,
+            required: [true, "Please provide company name"],
+            maxlength: 50,
+        },
+        position: {
+            type: String,
+            required: [true, "Please provide position"],
+            maxlength: 100,
+        },
+        status: {
+            type: String,
+            enum: ["interview", "declined", "pending"],
+            default: "pending",
+        },
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+            required: [true, "Please provide user"],
+        },
+        jobType: {
+            type: String,
+            enum: ["full-time", "part-time", "remote", "internship"],
+        },
+        jobLocation: {
+            type: String,
+            default: "my city",
+            required: [true, "Please provide city"],
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Job", JobSchema);
